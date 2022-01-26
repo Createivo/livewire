efdd1fdaaa069e3671418360d9b98c6be4606d87
= <kareem.force@gmail.com>
2022-01-26 23:59:27 +0200

    done

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -2,43 +2,70 @@
 
 namespace App\Http\Livewire;
 
+use App\Http\Livewire\Traits\Image;
 use App\Models\Posts as ModelsPosts;
 use Carbon\Carbon;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Posts extends Component
 {
-    public $posts  ;
-    public $title , $body ; // data received from the form inputs titel and body
+    use Image ;
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+
+    public $title , $body ; // data received from the form inputs title and body
 
     public function render() // construct like
     {
-        return view('livewire.posts');
+        return view('livewire.posts' , [
+            'posts' => ModelsPosts::latest() -> paginate(2)
+        ]);
     }
 
-    public function mount () { // runs when the livewire component calls
-        $this->posts = ModelsPosts::latest() -> get();
+    // make validation rules
+    public function rules(){
+        return [
+            'title' => 'required|max:30' ,
+            'body' => 'required'
+        ] ;
     }
+
+    // real time validation
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName); // take the rules from the rules method
+    }
+
 
     public function addPost(){ // eventListener function
 
-        if ($this->title == '' || $this->body == '') {
-            // if the title or the body is empty dont insert into db
-            return ;
-        }
+        $this -> validate(); // take the rules from the rules method
+
+        $img = $this -> uploadImageToServer();
 
         $created = ModelsPosts::create([
-            'title' => $this-> title , 
-            'body' => $this-> body
+            'title' => $this-> title ,
+            'body' => $this-> body ,
+            'img' => $img
         ]);
 
-        $this-> posts -> prepend($created);
+        $this -> body = '' ;
+        $this -> title = '' ;
+        $this -> image = '' ;
     }
 
     public function delete($id){
-        ModelsPosts::find($id)->delete();
-        $this->posts = ModelsPosts::latest() -> get();
+        try {
+            $post = ModelsPosts::find($id);
+            \Storage::disk('public')->delete(ModelsPosts::find($id)-> img);
+            $post -> delete();
+        } catch (\Exception $e) {
+            dd($e -> getMessage());
+        }
+
     }
 
-    
+
 }
