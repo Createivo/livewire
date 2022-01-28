830c5f78825ada0e0bd4177f5e166d0f19cfaea1
= <kareem.force@gmail.com>
2022-01-29 00:17:37 +0200

    modification;

--- app/Http/Livewire/SelectBox.php
+++ app/Http/Livewire/SelectBox.php
@@ -8,6 +8,8 @@ class SelectBox extends Component
 {
     public $cities = null; // first select box
     public $sideKick = [] ; // second select box
+
+    // run when the page rendered
     public function render()
     {
         return view('livewire.select-box' , [
@@ -15,6 +17,7 @@ class SelectBox extends Component
         ]);
     }
 
+    // fire when $cities property get updated [updated][Cities]
     public function updatedCities($value) {
 
         // this values will be added as option in second select box
